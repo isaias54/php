1849fd61443287ef8b8f73a974f0a206cf006cfc
isaias54 <rodriguesisaias774@gmail.com>
2017-05-03 15:58:41 -0300

    Add files via upload

---
+++ exemplo_isaias_9.php
@@ -0,0 +1,18 @@
+<Head>
+   <meta charset="utf-8">
+   <title>EXEMPLOS DE PHP</title>
+</Head>
+<body>
+<center>
+<h1>EXEMPLO 9 DO ISAÍAS</h1>
+</center>
+<?php
+for ($i = 1; $i <= 10; $i++)
+{
+echo("<b>Número: $i<br>");
+}
+   
+   
+?>
+</body>
+</html>
