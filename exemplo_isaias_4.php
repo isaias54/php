aaff6dcdc71ff26111e164a1002464beab9fdb4b
isaias54 <rodriguesisaias774@gmail.com>
2017-05-03 15:57:16 -0300

    Add files via upload

---
+++ exemplo_isaias_4.php
@@ -0,0 +1,21 @@
+<html>
+<Head>
+<meta charset="utf-8"/>
+<title>EXEMPLOS DE PHP</title>
+</Head>
+<body>
+<center>
+<h1>EXEMPLO 4 DO ISAÍAS</h1>
+</center>
+<?PHP
+echo"<h1>Operadoras de Strings</h1><br>";
+$a="bem vindo;";
+$b=" ao curso" ;
+$c=" de informática";
+$d=$a.$b.$c.="dá disciplina de php <br>";
+echo $d;
+$a.=" aluno";
+echo $a;
+?>
+</body>
+</html>
