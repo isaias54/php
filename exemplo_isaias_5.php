45274a774d772bc4302837f6778a674bce449747
isaias54 <rodriguesisaias774@gmail.com>
2017-05-03 15:57:28 -0300

    Add files via upload

---
+++ exemplo_isaias_5.php
@@ -0,0 +1,32 @@
+<html>
+<Head>
+<meta charset="utf-8"/>
+<title>EXEMPLOS DE PHP</title>
+</Head>
+<body>
+<center>
+<h1>EXEMPLO 5 DO ISAÍAS</h1>
+</center>
+<?php
+echo"<h1>Operadores de comparação</h1><br>";
+$a= 15;
+$b= 42;
+$c= 42.0;
+echo "$a==$c",$b==$c ?" verdadeiro" : "falso";
+echo "<br>";
+echo "$a===$c",$b===$c ?" verdadeiro" : " falso";
+echo "<br>";
+echo "$a!=$c",$b!=$c ?" verdadeiro" : " falso";
+echo "<br>";
+echo "$a<>$c",$b<>$c ?" verdadeiro" : " falso";
+echo "<br>";
+echo "$a<$c",$b<$c ?" verdadeiro" : " falso";
+echo "<br>";
+echo "$a>$c",$b>$c ?" verdadeiro" : " falso";
+echo "<br>";
+echo "$a<=$c",$b<=$c ?" verdadeiro" : " falso";
+echo "<br>";
+echo "$a>=$c",$b>=$c ?" verdadeiro" : " falso";
+?>
+</body>
+</html>
