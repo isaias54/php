f1f269afacdfd4b6871b85dd68ea2e882655639c
isaias54 <rodriguesisaias774@gmail.com>
2017-05-03 15:57:54 -0300

    Add files via upload

---
+++ exemplo_isaias_7_1.php
@@ -0,0 +1,21 @@
+<html>
+<Head>
+<meta charset="utf-8"/>
+<title>EXEMPLOS DE PHP </title>
+</Head>
+<body>
+<center>
+<h1>EXEMPLO 7.1 DO ISAÍAS</h1>
+</center>
+<?php
+$opc=2;
+if($opc==1){
+echo("<br><b>Opção escolhida 1</b>");
+}
+if($opc==2){
+echo("<br><b>Opção escolhida 2</b>");
+}
+
+?>
+</body>
+</html>
