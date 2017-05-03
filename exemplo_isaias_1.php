8a7e05e1b1d0f4e3690dbcc979eb6a9a907ef2ef
isaias54 <rodriguesisaias774@gmail.com>
2017-05-03 15:56:35 -0300

    Add files via upload

---
+++ exemplo_isaias_1.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+<title>EXEMPLOS DE PHP </title>
+</head>
+<body>
+<center>
+<h1>Exemplo 1 do isaias</h1>
+</center>
+<?php
+$idade=16;
+$nome="aluno do marly";
+echo "Olá meu nome é: ",$nome,"<br> Minha idade é: ",$idade;
+?>
+</body>
+</html>
