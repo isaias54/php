6fe57c589684b27b6be6ebad3cbb4aab7b9546b0
isaias54 <rodriguesisaias774@gmail.com>
2017-05-03 16:14:28 -0300

    Add files via upload

---
+++ exercicioPratico_isaias_4.php
@@ -0,0 +1,22 @@
+<title> isaías </title>
+<?php
+require_once "exercicioPratico03.html";
+$n1= $_POST["n1"];
+$n2= $_POST["n2"];
+$op= $_POST["op"];
+if($op=="Soma"){
+$soma= $n1+$n2;
+echo $soma;
+}else if($op=="Subtração"){
+$sub= $n1-$n2;
+echo $sub;
+}else if($op=="Multiplicação"){
+$mult= $n1*$n2;
+echo $mult;
+}else if($op=="Divisão"){
+$div= $n1/$n2;
+echo $div;
+}else{
+echo "Selecione a operação desejada";
+}
+?>
