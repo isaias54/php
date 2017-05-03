c27bae1b60ca59d1a23e7fe0e71b135b9eaa093e
isaias54 <rodriguesisaias774@gmail.com>
2017-05-03 15:56:49 -0300

    Add files via upload

---
+++ exemplo_isaias_2.php
@@ -0,0 +1,18 @@
+<html>
+<Head>
+<meta charset="utf-8"/>
+<title>EXEMPLOS DE PHP </title>
+</Head>
+<body>
+<center>
+<h1>EXEMPLO 2 DO ISAÍAS</h1>
+</center>
+<?php 
+echo "CONSTANTE DECLLARADAS";
+define("CONSTANTE_ALO","olá turma ! assim,declara uma constante");
+echo CONSTANTE_ALO;
+echo "<br><br><br>CONSTANTES DEFINIDAS<br>";
+echo phpinfo();
+?>
+</body>
+</html>
