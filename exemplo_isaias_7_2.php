c371f0591283d7d761cfdaf00367560cbc1a02db
isaias54 <rodriguesisaias774@gmail.com>
2017-05-03 15:58:05 -0300

    Add files via upload

---
+++ exemplo_isaias_7_2.php
@@ -0,0 +1,19 @@
+<html>
+<Head>
+<meta charset="utf-8"/>
+</Head>
+<body>
+<center>
+<h1>EXEMPLO 7.2 DO ISAÍAS</h1>
+</center>
+<?php
+$idade=12;
+$maioridade=18;
+if($idade>=$maioridade){
+echo ("<br><b>$idade Anos,Você já é Maior de Idade!</b>");}
+else{
+echo ("<br><b>$idade Anos, Você Ainda não é maior de idade!</b>");}
+
+?>
+</body>
+</html>
