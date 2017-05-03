dfa302ddc766607becf64dd763bd3f9858a3bb61
isaias54 <rodriguesisaias774@gmail.com>
2017-05-03 15:58:53 -0300

    Add files via upload

---
+++ exemplo_isaias_10.php
@@ -0,0 +1,18 @@
+<html>
+<Head>
+<meta charset="utf-8">
+<title>EXEMPLOS DE PHP</title>
+</Head>
+<body>
+<center>
+<h1>EXEMPLO 10 DO ISAÍAS</h1>
+</center>
+<?php
+$num=1;
+while($num<=10){
+echo ("número:$num<br>");
+$num++;
+}
+?>
+</body>
+</html>
